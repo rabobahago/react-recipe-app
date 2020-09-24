7ca49f9fc57ca6f694f4563b54ef7453e96d9b08
rabobahago <rabobahago@gmail.com>
2020-09-24 10:06:26 +0100

    styles added

--- src/App.js
+++ src/App.js
@@ -7,12 +7,13 @@ const App = () => {
   const APP_KEY = '6c8e5b86c044e12fd51b5e729f864c32'
   const [recipes, setRecipes] = useState([])
   const [search, setSearch] = useState('')
+  const [query, setQuery] = useState('chicken')
   useEffect(() => {
     getRecipes()
-  }, [])
+  }, [query])
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`,
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`,
     )
     const data = await response.json()
     setRecipes(data.hits)
@@ -20,9 +21,14 @@ const App = () => {
   const updateSearch = (e) => {
     setSearch(e.target.value)
   }
+  const getSearch = (e) => {
+    e.preventDefault()
+    setQuery(search)
+    setSearch('')
+  }
   return (
     <div className="App">
-      <form className="search-form">
+      <form onSubmit={getSearch} className="search-form">
         <input
           className="search-bar"
           type="text"
@@ -33,16 +39,19 @@ const App = () => {
           Search
         </button>
       </form>
-      {recipes.map((recipe) => {
-        return (
-          <Recipe
-            key={recipe.recipe.label}
-            title={recipe.recipe.label}
-            calories={recipe.recipe.calories}
-            image={recipe.recipe.image}
-          />
-        )
-      })}
+      <div className="recipes">
+        {recipes.map((recipe) => {
+          return (
+            <Recipe
+              key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+              ingredients={recipe.recipe.ingredients}
+            />
+          )
+        })}
+      </div>
     </div>
   )
 }
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,11 +1,17 @@
 import React from 'react'
+import styles from './recipe.module.css'
 
-const Recipe = ({ title, calories, image }) => {
+const Recipe = ({ title, calories, image, ingredients }) => {
   return (
-    <div>
+    <div className={styles.recipe}>
       <h1>{title}</h1>
+      <ol>
+        {ingredients.map((ingredient) => {
+          return <li>{ingredient.text}</li>
+        })}
+      </ol>
       <p>{calories}</p>
-      <img src={image} alt="" />
+      <img className={styles.image} src={image} alt="" />
     </div>
   )
 }
